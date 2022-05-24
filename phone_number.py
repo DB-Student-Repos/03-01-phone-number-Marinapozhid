c53e221b3df0709221ed3f428e01d8be764e464b
Marina <m.pozhidayeva@gmail.com>
2022-05-24 13:15:28 +0200

    nbvn

--- phone_number.py
+++ phone_number.py
@@ -1,3 +1,16 @@
+from re import search
 class PhoneNumber:
     def __init__(self, number):
+        self.number = self.clean_punctuation(number)
+
+    def clean_punctuation(self, number):
+        no_punctuation = list(filter(str.isdigit, list(number)))
+        if len(no_punctuation) == 11 and no_punctuation[0] == '1':
+            no_punctuation.remove('1')
+        return no_punctuation
+
+    def check_validity(self, number):
         pass
+
+ 
+
